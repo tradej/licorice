5d3cdd6340036d018d11dea5ec350fcfe096a3d7
Tomas Radej <tradej@redhat.com>
2013-12-19 15:46:33 +0100

    Removed requirement for virtualenv to be present

--- test-env.sh
+++ test-env.sh
@@ -3,16 +3,14 @@
 # This is a script that builds and installs licorice into virtualenv for
 # quick testing
 
-if [ ! -f ~/.local/bin/virtualenv-3.3 ]; then exit; fi
-
 rm -rf env dist *.egg-info
-virtualenv-3.3 env
+pyvenv-3.3 env
 ./setup.py sdist
 
 cp dist/*.gz env
 cd env
 source bin/activate
-easy_install ./*.gz
+easy_install ./*.gz --user
 
 sh
 
