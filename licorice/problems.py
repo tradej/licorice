18c6f2cdfc9e0d51d51f5df6d31f9fe9a54bae5a
Tomas Radej <tomas@radej.cz>
2013-12-23 18:02:57 +0100

    Problems

--- licorice/problems.py
+++ licorice/problems.py
@@ -8,42 +8,52 @@ class ProblemAnalyzer:
     '''
     Checks for problems:
     * Binary files found
-    * No main license file found (applicable to archives and directories)
-    * No licensing information found
-    * Multiple licenses found
     * Non-free license found
     * Unknown license found
-    * Different licenses from main license found
-    * Two copyleft licenses found
-    * Main licenses is non-copyleft, found copyleft
     '''
 
     def __init__(self, project):
         self.project = project
 
-    def _get_main_license_files(self):
+    def _get_main_licenses(self):
         names = map(re.compile, ['licen[cs]e', 'copyright', 'copying'])
         results = set()
         for f in self.project.files:
             for n in names:
                 if n.search(f.filename.lower()) and f.licenses:
-                    results.add(f)
+                    results |= set(f.licenses)
         return results
 
+    def problem_multiple_licenses_found(self):
+        return (len(self.project.licenses) > 1, 'Found more than 1 license: {}'.format(\
+                ', '.join([l.name for l in self.project.licenses])))
+
     def problem_no_main_license_file(self):
-        return (not bool(self._get_main_license_files()), 'No main license file found')
+        return (not bool(self._get_main_licenses()), 'No main license file found')
 
     def problem_no_licensing_information_found(self):
         return (not self.project.licenses, 'No licensing information found')
 
-#    def problem_other_licenses_from_main_found(self):
-#        main_licenses = set()
-#        for f in self._get_main_license_files():
-#            for l in f.licenses:
-#                main_licenses.add(l)
-#
-#        diff = main_licenses.difference(self.project.licenses)
-#        return (bool(diff), 'Additional licenses to the main one found: {}'.format(', '.join(diff)))
+    def problem_different_licenses_from_main_found(self):
+        main_licenses = self._get_main_licenses()
+        diff = self.project.licenses.difference(main_licenses)
+        return (bool(diff), 'Found licenses different from the main license: {}'.format(\
+                ', '.join([l.name for l in diff])))
+
+    def problem_multiple_copyleft_licenses_found(self):
+        copyleft_found = False
+        for lic1 in self.project.licenses:
+            if lic1.restrictions['copyleft']:
+                if not copyleft_found: copyleft_found = True
+                else: return (True, 'Multiple copyleft licenses found')
+        return (False, None)
+
+    def problem_non_copyleft_main_license(self):
+        main_licenses = self._get_main_licenses()
+        if False in [f.restrictions['copyleft'] for f in main_licenses]:
+            if True in [l.restrictions['copyleft'] for l in self.project.licenses]:
+                return (True, 'Copyleft license found, but main license is non-copyleft. Please check if relicensing is necessary')
+        return (False, None)
 
     def problem_incompatible_licenses_found(self):
         results = list()
@@ -58,7 +68,7 @@ class ProblemAnalyzer:
         for name in dir(self):
             attribute = getattr(self, name)
             if ismethod(attribute) and attribute.__name__.startswith('problem'):
-                result, comment = attribute()
+                (result, comment) = attribute()
                 if result: results.append(comment)
 
         logger.debug('Detected {} problems'.format(len(results)))
